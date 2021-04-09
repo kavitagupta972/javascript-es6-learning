0e33979269bfa2f2412616b376e9dcf0ce95c996
kavitagupta972 <kavitagupta972@gmail.com>
2021-04-09 07:10:31 +0530

    Added module implementation

---
+++ ES6-concepts/calculator.js
@@ -0,0 +1,19 @@
+
+
+var add  =  function(a,b) {
+    return a+b;
+}
+
+var subtract = function(a,b) {
+    return a-b;
+}
+
+var multiplication = function(a,b){
+    return a*b;
+}
+
+var division = function(a,b){
+    return a/b;
+}
+
+export {add, subtract, multiplication, division};
---
+++ ES6-concepts/moduleDemo.js
@@ -0,0 +1,8 @@
+import {add, subtract, multiplication, division} from "./calculator.js";
+
+var x = 10, y = 5;
+
+console.log("sum is ", add(x,y));
+console.log("subtraction is ", subtract(x,y));
+console.log("multiplication is ", multiplication(x,y));
+console.log("division is ", division(x,y));
